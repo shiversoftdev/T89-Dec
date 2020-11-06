14096cf5e3f4a0763e6f74c4065c560f29752a09
s <s>
2020-11-05 18:36:26 -0600

    Add project files.

---
+++ Cerberus.Logic/Decompiler/DecompilerBlocks/TernaryBlock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerberus.Logic
+{
+    /// <summary>
+    /// A class to hold an if Block
+    /// </summary>
+    internal class TernaryBlock : DecompilerBlock
+    {
+        /// <summary>
+        /// The comparison being made
+        /// </summary>
+        public string Comparison { get; set; }
+
+        public string TrueCondition { get; set; }
+
+        public string FalseCondition { get; set; }
+
+        public bool IsJumpOnTrue { get; set; }
+
+        /// <summary>
+        /// Initializes an if Block
+        /// </summary>
+        public TernaryBlock(int startOffset, int endOffset) : base(startOffset, endOffset) { RequiresBraces = false; }
+
+        /// <summary>
+        /// Gets the header
+        /// </summary>
+        public override string GetHeader() => string.Format(IsJumpOnTrue ? "({0} ? {2} : {1})" : "({0} ? {1} : {2})", Comparison, TrueCondition, FalseCondition);
+
+        /// <summary>
+        /// Gets the footer
+        /// </summary>
+        public override string GetFooter() => null;
+    }
+}
---
+++ Cerberus.UI/AboutWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cerberus.UI
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DonateButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void HomePageButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
